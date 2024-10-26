f81d31ee22eb707cb7d8001a0255e585cc1d5354
Taisei Yamane <yamatai19981212@gmail.com>
2024-10-26 10:57:04 +0900

    laravel-command

---
+++ example-app/app/Console/Commands/SampleCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SampleCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sample-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('handle method is calld');
+    }
+}
---
+++ example-app/routes/console.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('easy-command', function () {
+    $this->info('This is an easy command');
+});
